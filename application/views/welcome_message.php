a8d8008acc99e491de91e4f15dd38250333ff7aa
Alejo5600 <alejo5600@gmail.com>
2019-10-03 20:50:37 -0500

    app

---
+++ application/views/welcome_message.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Hola esta es una prueba</h1>
+	<h2> <?php echo $Mensaje ?> </h2>
+	<h3> El color seleccionado es <?php echo $Color?></h3>
+</body>
+</html>
